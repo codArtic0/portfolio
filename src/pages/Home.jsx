b5dff8fcc23649c06dfea4a534adbdd545be8883
codArtic0 <raulsousadesigner@gmail.com>
2026-04-15 16:55:28 -0300

    Add: Logos interativas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,12 @@ import "../styles/Home.css"
 export default function Home(){
     return(
         <div className="home-container">
-            <div className="title">
-                <h1>Prazer, eu me chamo <strong className="portfolio-word">Raul Sousa</strong>!</h1>
+            <div className="title-elements">
+                <div className="title">
+                    <h1>Prazer, eu me chamo <strong className="portfolio-word">Raul Sousa</strong>!</h1>
+                </div>
+                <img className='logo-js' src="/jsLogo.png" alt="" />
+                <img className="logo-java" src="javaLogo.png" alt=""/>
             </div>
             <div className="name-logo">
                 <div className="bg-photo">
