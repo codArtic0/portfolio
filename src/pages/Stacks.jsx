05c328c347109ee02891cd1f4525ec029b7a92ce
codArtic0 <raulsousadesigner@gmail.com>
2026-04-22 12:37:33 -0300

    ADD: Stacks

--- src/pages/Stacks.jsx
+++ src/pages/Stacks.jsx
@@ -4,7 +4,33 @@ export default function Stacks(){
     return(
         <>
             <div className="stacks-container">
-                <h1>Hello</h1>
+                <div className="group-all-stacks">
+                    <div className="card">
+                        <div className="group-stacks">
+                            <div className="group-stack-element">
+                                <h1 className="title-stack">REACT.JS</h1>
+                            </div>
+                        </div>
+                        
+                    </div>
+                        <div className="card">
+                            <h1 className="title-stack">BACK-END</h1>
+                            <div className="group-stacks">
+                            <div className="group-stack-element">
+                                <img src="/jsLogo.webp" alt="Logo JavaScript" />
+                                <h1>JAVASCRIPT</h1>
+                            </div>
+                            <div className="group-stack-element">
+                                <img src="/javaLogo.webp" alt="Logo JavaScript" />
+                                <h1>JAVA</h1>
+                            </div>
+                            <div className="group-stack-element">
+                                <img src="/javaLogo.webp" alt="Logo Python" />
+                                <h1>PYTHON</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
