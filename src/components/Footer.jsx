adf95a249e0a19bc4266d13aec4552cab6cc2ea3
codArtic0 <raulsousadesigner@gmail.com>
2026-04-30 14:14:05 -0300

    ADD: Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,23 @@
+import "../styles/Footer.css"
+
 export default function Footer(){
     return(
         <div className="footer-container">
-            <h1>Footer</h1>
+            <div className="footer-content-container">
+                <div className="logo-name">
+                    <h1 className="name">Raul Sousa</h1>
+                </div>
+                <div className="direitos">
+                    <p>© 2024 Raul Sousa. Todos os direitos reservados.</p>
+                </div>
+                <div className="menu">
+                    <ul>
+                        <li>Github</li>
+                        <li>Linkedin</li>
+                        <li>Instagram</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
