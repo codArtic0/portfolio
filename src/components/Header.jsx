24353b96b9c71d7373e355150be240e9290010ca
codArtic0 <raulsousadesigner@gmail.com>
2026-04-30 10:48:11 -0300

    STYLE: Change font-size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <header className="header-container">
             <div className="header-content-container">
-                <h1>Raul Sousa</h1>
+                <h1 className="name">Raul Sousa</h1>
             </div>
         </header>
     )
