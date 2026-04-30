0c0266377cacbf83f0a45d4dc2d08c8b876a6b9f
codArtic0 <raulsousadesigner@gmail.com>
2026-04-30 10:52:08 -0300

    ADD: Monitor PlaceHolder

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,20 @@ import Button from "../components/Button"
 export default function Home() {
     return (
         <div className="home-container">
-            <div className="disponivel">
-                <p>Disponível para contratação</p>
+            <div className="txt-container">
+                <div className="disponivel">
+                    <p>Disponível para contratação</p>
+                </div>
+                <h1>Construindo soluções para sua empresa através de sites</h1>
+                <p>Construo sites com alta perfomance, e com as tecnologias mais atuais do mercado. Com SEO bem definido e site focado em experiência de usuário e design</p>
+                <div className="button-group">
+                    <Button variant="primary"> Contratar Agora </Button>
+                    <Button variant="secondary"> Veja Meus Trabalhos </Button>
+                </div>
             </div>
-            <h1>Construindo soluções para sua empresa através de sites</h1>
-            <p>Construo sites com alta perfomance, e com as tecnologias mais atuais do mercado. Com SEO bem definido e site focado em experiência de usuário e design</p>
-            <div className="button-group">
-                <Button variant="primary"> Contratar agora </Button>
-                <Button variant="secondary"> Veja meus trabalhos </Button>
-            </div>
+            <div className="img-container">
+                    <img src="monitor.webp" alt="Monitor Apple"/>
+                </div>
         </div>
     )
 }
