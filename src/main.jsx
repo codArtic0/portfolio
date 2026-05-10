ec56a34532d76d86974a1198f80c57e06cd98b44
codArtic0 <raulsousadesigner@gmail.com>
2026-05-10 17:19:34 -0300

    FIX: Unused import removed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './pages/App.jsx'
-import React from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
