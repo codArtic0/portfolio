45b69f1d7de94edb23b0133b143b1faa0c7e9d90
codArtic0 <raulsousadesigner@gmail.com>
2026-05-10 22:20:14 -0300

    FIX: CTA's, Titles and Styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,12 +9,12 @@ export default function Home() {
                 <div className="disponivel">
                     <p>Disponível para contratação</p>
                 </div>
-                <h1>Construindo soluções para sua empresa através de sites</h1>
+                <h1>Desenvolvendo aplicações Full-Stack escaláveis com foco em performance e experiência do usuário</h1>
                 <div className="paragraph-description">
-                    <p>Construo sites com alta perfomance, e com as tecnologias mais atuais do mercado. Com SEO bem definido e site focado em experiência de usuário e design.</p>
+                    <p>Unindo 6 anos de Design Gráfico + Formação em Ciência da Computação, construo sites com alta perfomance, e com as tecnologias mais atuais do mercado. Com SEO bem definido e site focado em experiência de usuário e design.</p>
                 </div>
                 <div className="button-group">
-                    <a href="https://wa.me/5587999967672?text=Ol%C3%A1!%20Vim%20pelo%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20comprar%20um%20site!" target="_blank" rel="noopener noreferrer"><Button variant="primary"> Contratar Agora </Button></a>
+                    <a href="https://wa.me/5587999967672?text=Ol%C3%A1!%20Vim%20pelo%20seu%20portf%C3%B3lio%20e%20gostaria%20de%20comprar%20um%20site!" target="_blank" rel="noopener noreferrer"><Button variant="primary"> Ver orçamento </Button></a>
                     <Button variant="secondary"> Veja Meus Trabalhos</Button>
                 </div>
             </div>
